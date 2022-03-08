d64852e388c257c364e349b1ca498b2de05b0b18
Rylexium <aleksei.azizov@mail.ru>
2022-03-08 16:09:48 +0400

    coroutine in splash

--- app/src/main/java/com/example/vkr/splash_screen/SplashScreen.kt
+++ app/src/main/java/com/example/vkr/splash_screen/SplashScreen.kt
@@ -9,7 +9,6 @@ import android.os.Handler
 import android.os.Looper
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.MotionEvent
 import android.widget.ImageView
 import android.widget.LinearLayout
 import com.androidnetworking.AndroidNetworking
@@ -22,14 +21,17 @@ import com.example.vkr.activity.authorization.AuthorizationActivity
 import com.example.vkr.utils.OpenActivity
 import com.example.vkr.utils.ShowToast
 import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.json.JSONArray
 import org.json.JSONObject
 import java.lang.Exception
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 import kotlin.random.Random
 
 class SplashScreen : AppCompatActivity() {
-
-    private var isConnect : Boolean = false
 
     companion object {
         lateinit var sharedPreferences : SharedPreferences
@@ -65,91 +67,87 @@ class SplashScreen : AppCompatActivity() {
 
         val view = findViewById<LinearLayout>(R.id.splash_layout)
         view.alpha = 0f
-        checkConnect()
         Log.e("", sharedPreferences.all.toString())
         view.animate().setDuration(Random.nextInt(1000, 2500).toLong()).alpha(1f).withEndAction{
-            Thread {
-                while (!isConnect) { } //ждём когда коннект будет установлен
+            GlobalScope.launch {
+
+                run tryToConnect@{
+                    repeat(1001) {
+                        val connect = awaitConnect()
+                        if (connect) return@tryToConnect
+                        if (it % 7 == 0) Handler(Looper.getMainLooper()).post {
+                            ShowToast.show(
+                                this@SplashScreen,
+                                "Проверьте подключение к интернету"
+                            )
+                        }
+                        delay(1000)
+                    }
+                }
                 authorization()
-            }.start()
+                return@launch
+            }
         }
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        checkConnect()
-        return true
+
+    private suspend fun awaitConnect() : Boolean {
+        return suspendCoroutine {
+            AndroidNetworking.get("https://vkr1-app.herokuapp.com/nationality")
+                .setPriority(Priority.IMMEDIATE)
+                .build()
+                .getAsJSONArray(object : JSONArrayRequestListener {
+                    override fun onResponse(response: JSONArray) { it.resume(true) }
+                    override fun onError(anError: ANError?) { it.resume(false) }
+                })
+        }
+
     }
 
-    private fun checkConnect(){
-        val activity = this
-        val mainHandler = Handler(Looper.getMainLooper())
-        var count = 0
-        mainHandler.post(object : Runnable {
-            override fun run() {
-                AndroidNetworking.get("https://vkr1-app.herokuapp.com/nationality")
-                    .setPriority(Priority.IMMEDIATE)
-                    .build()
-                    .getAsJSONArray(object : JSONArrayRequestListener {
-                        override fun onResponse(response: JSONArray) {
-                            isConnect = true
-                        }
-
-                        override fun onError(anError: ANError?) {
-                            if(count % 7 == 0) ShowToast.show(activity, "Проверьте подключение к интернету")
-                        }
-                    })
-                count++
-                if(isConnect)
-                    mainHandler.removeCallbacksAndMessages(null)
-                else
-                    mainHandler.postDelayed(this, 1000)
-            }
-        })
-    }
-
-    private fun authorization(){
-        val activity = this
-        AndroidNetworking.get(
-            String.format(
-                "https://vkr1-app.herokuapp.com/authorization?login=%s&password=%s",
-                sharedPreferences.all["login"], sharedPreferences.all["password"]
+    private suspend fun authorization(){
+        return suspendCoroutine {
+            AndroidNetworking.get(
+                String.format(
+                    "https://vkr1-app.herokuapp.com/authorization?login=%s&password=%s",
+                    sharedPreferences.all["login"], sharedPreferences.all["password"]
+                )
             )
-        )
-            .setPriority(Priority.IMMEDIATE)
-            .build()
-            .getAsJSONObject(object : JSONObjectRequestListener {
-                override fun onResponse(response: JSONObject) {
-                    try {
-                        val jsonNode = ObjectMapper().readTree(response.toString())
-                        if (jsonNode["idEducation"] == null) {
-                            throw Exception("idEducation is null")
-                        } else if (jsonNode["idEducation"].toString().toInt() < 5
-                            && !java.lang.Boolean.parseBoolean(jsonNode["user"]["is_entry"].toString())
-                        ) {
-                            OpenActivity.openExamsResult(
-                                activity,
-                                jsonNode["user"]["id_abit"].toString(),
-                                jsonNode["user"]["login"].toString().replace("\"", ""),
-                                jsonNode["idEducation"].toString()
-                            )
-                        } else {
-                            OpenActivity.openPersonalCabinet(
-                                activity,
-                                jsonNode["user"]["id_abit"].toString(),
-                                jsonNode["user"]["login"].toString().replace("\"", ""),
-                                jsonNode["idEducation"].toString()
-                            )
+                .setPriority(Priority.IMMEDIATE)
+                .build()
+                .getAsJSONObject(object : JSONObjectRequestListener {
+                    override fun onResponse(response: JSONObject) {
+                        try {
+                            val jsonNode = ObjectMapper().readTree(response.toString())
+                            if (jsonNode["idEducation"] == null) {
+                                throw Exception("idEducation is null")
+                            } else if (jsonNode["idEducation"].toString().toInt() < 5
+                                && !java.lang.Boolean.parseBoolean(jsonNode["user"]["is_entry"].toString())
+                            ) {
+                                OpenActivity.openExamsResult(
+                                    this@SplashScreen,
+                                    jsonNode["user"]["id_abit"].toString(),
+                                    jsonNode["user"]["login"].asText(),
+                                    jsonNode["idEducation"].toString()
+                                )
+                            } else {
+                                OpenActivity.openPersonalCabinet(
+                                    this@SplashScreen,
+                                    jsonNode["user"]["id_abit"].toString(),
+                                    jsonNode["user"]["login"].asText(),
+                                    jsonNode["idEducation"].toString()
+                                )
+                            }
+                        } catch (e: Exception) {
+                            startActivity(Intent(this@SplashScreen, AuthorizationActivity::class.java))
+                            finish()
                         }
-                    } catch (e: Exception) {
-                        startActivity(Intent(activity, AuthorizationActivity::class.java))
+                    }
+
+                    override fun onError(error: ANError) {
+                        startActivity(Intent(this@SplashScreen, AuthorizationActivity::class.java))
                         finish()
                     }
-                }
-
-                override fun onError(error: ANError) {
-                    startActivity(Intent(activity, AuthorizationActivity::class.java))
-                    finish()
-                }
-            })
+                })
+        }
     }
 }
