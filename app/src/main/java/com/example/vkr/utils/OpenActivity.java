e443b8b34ccdf79728f36cf353558a88b82206a8
Rylexium <aleksei.azizov@mail.ru>
2022-03-17 21:10:59 +0400

    add maps

--- app/src/main/java/com/example/vkr/utils/OpenActivity.java
+++ app/src/main/java/com/example/vkr/utils/OpenActivity.java
@@ -30,15 +30,20 @@ public class OpenActivity {
     }
 
     public static boolean openMapsWhereWe(Activity activity){
-//        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
-//        int resultCode = googleApiAvailability.isGooglePlayServicesAvailable(activity);
-//        if(resultCode == ConnectionResult.SUCCESS)
-//            activity.startActivity(new Intent(activity, GoogleMapsActivity.class));
-//        else
-            activity.startActivity(new Intent(activity, YandexMapsActivity.class));
-//        activity.startActivity(new Intent(Intent.ACTION_VIEW)
-//                .setData(Uri.parse("https://www.google.ru/maps/place/53%C2%B012'49.3%22N+50%C2%B010'37.3%22E/@53.2137071," +
-//                        "50.1762502,18z/data=!3m1!4b1!4m6!3m5!1s0x0:0xeeb6557ca0cc4e6b!7e2!8m2!3d53.2137058!4d50.1770238?hl=ru")));
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = googleApiAvailability.isGooglePlayServicesAvailable(activity);
+        if(resultCode == ConnectionResult.SUCCESS)
+            activity.startActivity(new Intent(activity, GoogleMapsActivity.class));
+        else
+            try {
+                activity.startActivity(new Intent(activity, YandexMapsActivity.class));
+            }
+            catch (Exception ignored){
+                activity.startActivity(new Intent(Intent.ACTION_VIEW)
+                    .setData(Uri.parse("https://www.google.ru/maps/place/53%C2%B012'49.3%22N+50%C2%B010'37.3%22E/@53.2137071," +
+                            "50.1762502,18z/data=!3m1!4b1!4m6!3m5!1s0x0:0xeeb6557ca0cc4e6b!7e2!8m2!3d53.2137058!4d50.1770238?hl=ru")));
+            }
+
         return true;
     }
 
