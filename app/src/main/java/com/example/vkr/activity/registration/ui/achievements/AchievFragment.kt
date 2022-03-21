b6497f01bafd57b557174bf1c539f9c18568ab57
Rylexium <aleksei.azizov@mail.ru>
2022-03-21 18:38:12 +0400

    async in registration

--- app/src/main/java/com/example/vkr/activity/registration/ui/achievements/AchievFragment.kt
+++ app/src/main/java/com/example/vkr/activity/registration/ui/achievements/AchievFragment.kt
@@ -10,11 +10,16 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.view.get
 import androidx.lifecycle.lifecycleScope
+import com.example.vkr.R
 import com.example.vkr.activity.registration.RegistrationActivity
 import com.example.vkr.databinding.FragmentAchievBinding
 import com.example.vkr.utils.ConvertClass
@@ -30,17 +35,6 @@ class AchievFragment: Fragment() {
     private val binding get() = _binding!!
     private var sharedPreferences : SharedPreferences? = null
     val KEY_ACHIEVEMENT = "achievement_bitmap"
-
-    var bitmap1 : Bitmap? = null
-    var bitmap2 : Bitmap? = null
-    var bitmap3 : Bitmap? = null
-    var bitmap4 : Bitmap? = null
-    var bitmap5 : Bitmap? = null
-
-    companion object {
-        @JvmStatic var PIC_CODE : Int = 0
-    }
-
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -71,52 +65,52 @@ class AchievFragment: Fragment() {
                 SelectImageClass.CAMERA -> bitmap = BitmapFactory.decodeFile(SelectImageClass.currentPhotoPath)
                 SelectImageClass.GALLERY -> bitmap = ConvertClass.decodeUriToBitmap(context, data?.data)
             }
-            if (bitmap != null) {
-                when (PIC_CODE){
-                    0 -> bitmap1 = bitmap
-                    1 -> bitmap2 = bitmap
-                    2 -> bitmap3 = bitmap
-                    3 -> bitmap4 = bitmap
-                    4 -> bitmap5 = bitmap
-                }
+            if (bitmap != null)
                 EditLinearLayout.onAddField(bitmap, binding.layoutForImagesAchievements, activity)
-            }
-            if(PIC_CODE == 4) PIC_CODE = 0
-            else PIC_CODE += 1
         }
     }
 
     private suspend fun comebackAfterOnBackPressed() {
         return coroutineScope {
             val restoredText1 = sharedPreferences!!.getString(KEY_ACHIEVEMENT + "0", null)
-            if(restoredText1 != null && restoredText1 != "")
+            if(restoredText1 != null && restoredText1 != ""){
+                val bitmap = ConvertClass.convertStringToBitmap(restoredText1)
                 Handler(Looper.getMainLooper()).post {
-                    EditLinearLayout.onAddField(ConvertClass.convertStringToBitmap(restoredText1), binding.layoutForImagesAchievements, activity)
+                    EditLinearLayout.onAddField(bitmap, binding.layoutForImagesAchievements, activity)
                 }
+            }
 
             val restoredText2 = sharedPreferences!!.getString(KEY_ACHIEVEMENT + "1", null)
-            if(restoredText2 != null && restoredText2 != "")
+            if(restoredText2 != null && restoredText2 != ""){
+                val bitmap = ConvertClass.convertStringToBitmap(restoredText2)
                 Handler(Looper.getMainLooper()).post {
-                    EditLinearLayout.onAddField(ConvertClass.convertStringToBitmap(restoredText2), binding.layoutForImagesAchievements, activity)
+                    EditLinearLayout.onAddField(bitmap, binding.layoutForImagesAchievements, activity)
                 }
+            }
 
             val restoredText3 = sharedPreferences!!.getString(KEY_ACHIEVEMENT + "2", null)
-            if(restoredText3 != null && restoredText3 != "")
+            if(restoredText3 != null && restoredText3 != ""){
+                val bitmap = ConvertClass.convertStringToBitmap(restoredText3)
                 Handler(Looper.getMainLooper()).post {
-                    EditLinearLayout.onAddField(ConvertClass.convertStringToBitmap(restoredText3), binding.layoutForImagesAchievements, activity)
+                    EditLinearLayout.onAddField(bitmap, binding.layoutForImagesAchievements, activity)
                 }
+            }
 
             val restoredText4 = sharedPreferences!!.getString(KEY_ACHIEVEMENT + "3", null)
-            if(restoredText4 != null && restoredText4 != "")
+            if(restoredText4 != null && restoredText4 != ""){
+                val bitmap = ConvertClass.convertStringToBitmap(restoredText4)
                 Handler(Looper.getMainLooper()).post {
-                    EditLinearLayout.onAddField(ConvertClass.convertStringToBitmap(restoredText4), binding.layoutForImagesAchievements, activity)
+                    EditLinearLayout.onAddField(bitmap, binding.layoutForImagesAchievements, activity)
                 }
+            }
 
             val restoredText5 = sharedPreferences!!.getString(KEY_ACHIEVEMENT + "4", null)
-            if(restoredText5 != null && restoredText5 != "")
+            if(restoredText5 != null && restoredText5 != ""){
+                val bitmap = ConvertClass.convertStringToBitmap(restoredText5)
                 Handler(Looper.getMainLooper()).post {
-                    EditLinearLayout.onAddField(ConvertClass.convertStringToBitmap(restoredText5), binding.layoutForImagesAchievements, activity)
+                    EditLinearLayout.onAddField(bitmap, binding.layoutForImagesAchievements, activity)
                 }
+            }
             Handler(Looper.getMainLooper()).post{
                 RegistrationActivity.next.isEnabled = true
                 RegistrationActivity.previous.isEnabled = true
@@ -132,26 +126,11 @@ class AchievFragment: Fragment() {
             sharedPreferences!!.edit().putString(KEY_ACHIEVEMENT + i, "").apply()
 
         return coroutineScope {
-            if(bitmap1 != null)
+            if(binding.layoutForImagesAchievements.childCount == 0) return@coroutineScope
+            for(i in 0 until binding.layoutForImagesAchievements.childCount)
                 sharedPreferences!!.edit()
-                    .putString(KEY_ACHIEVEMENT + "0", ConvertClass.convertBitmapToString(bitmap1))
-                    .apply()
-            if(bitmap2 != null)
-                sharedPreferences!!.edit()
-                    .putString(KEY_ACHIEVEMENT + "1", ConvertClass.convertBitmapToString(bitmap2))
-                    .apply()
-            if(bitmap3 != null)
-                sharedPreferences!!.edit()
-                    .putString(KEY_ACHIEVEMENT + "2", ConvertClass.convertBitmapToString(bitmap3))
-                    .apply()
-            if(bitmap4 != null)
-                sharedPreferences!!.edit()
-                    .putString(KEY_ACHIEVEMENT + "3", ConvertClass.convertBitmapToString(bitmap4))
-                    .apply()
-            if(bitmap5 != null)
-                sharedPreferences!!.edit()
-                    .putString(KEY_ACHIEVEMENT + "4", ConvertClass.convertBitmapToString(bitmap5))
-                    .apply()
+                    .putString(KEY_ACHIEVEMENT + i, ConvertClass.convertBitmapToString(binding.layoutForImagesAchievements[i]
+                        .findViewById<ImageView>(R.id.image_edit).drawable.toBitmap())).apply()
         }
     }
 
