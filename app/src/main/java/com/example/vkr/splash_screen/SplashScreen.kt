7b8064fc966e0667d78a81a745dc846f905a0d53
Rylexium <aleksei.azizov@mail.ru>
2022-03-22 22:43:36 +0400

    splash new themes

--- app/src/main/java/com/example/vkr/splash_screen/SplashScreen.kt
+++ app/src/main/java/com/example/vkr/splash_screen/SplashScreen.kt
@@ -1,14 +1,17 @@
 package com.example.vkr.splash_screen
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.ImageView
-import android.widget.LinearLayout
+import android.view.Gravity
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.common.Priority
@@ -22,6 +25,7 @@ import com.example.vkr.activity.authorization.AuthorizationActivity
 import com.example.vkr.utils.OpenActivity
 import com.example.vkr.utils.ShowToast
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.json.JSONArray
@@ -30,53 +34,28 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 import kotlin.random.Random
 
-class SplashScreen : AppCompatActivity() {
 
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
     companion object {
         lateinit var sharedPreferences : SharedPreferences
     }
+    private lateinit var logoImageView : ImageView
+    private lateinit var mainLayout : LinearLayout
+    private lateinit var viewLayout : LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen_activity)
-        val logoImageView = findViewById<ImageView>(R.id.splash_logo)
-        val mainLayout = findViewById<LinearLayout>(R.id.splash_layout_main)
-        // FIXME: подумать над кодировкой и добавить фото Ксюши, и тэг "сам знаешь кто здесь должен быть" (mini97)
-        // FIXME: подумать над вероятностью появления каждой картинки
-        // FIXME: sharedPreference в registration сделать общий 
-        when(Random.nextInt(1, 4)){
-            1 -> {
-                mainLayout.setBackgroundColor(applicationContext.getColor(R.color.color_for_splash_screen1))
-                Glide.with(this)
-                    .load(R.drawable.splash_screen1)
-                    .format(DecodeFormat.PREFER_RGB_565)
-                    .into(logoImageView)
-                window.navigationBarColor = applicationContext.getColor(R.color.blue_700)
-                window.statusBarColor = applicationContext.getColor(R.color.blue_700)
-            }
-            2 -> {
-                mainLayout.setBackgroundColor(applicationContext.getColor(R.color.color_for_splash_screen2))
-                Glide.with(this)
-                    .load(R.drawable.splash_screen2)
-                    .format(DecodeFormat.PREFER_RGB_565)
-                    .into(logoImageView)
-                window.navigationBarColor = Color.parseColor("#000080")
-                window.statusBarColor = Color.parseColor("#000080")
-            }
-            3 -> {
-                mainLayout.setBackgroundColor(applicationContext.getColor(R.color.color_for_splash_screen3))
-                Glide.with(this)
-                    .load(R.drawable.splash_screen3)
-                    .format(DecodeFormat.PREFER_RGB_565)
-                    .into(logoImageView)
-                window.navigationBarColor = applicationContext.getColor(R.color.orange_700)
-                window.statusBarColor = applicationContext.getColor(R.color.orange_700)
-            }
-        }
+
+        logoImageView = findViewById(R.id.splash_logo)
+        mainLayout = findViewById(R.id.splash_layout_main)
+        viewLayout = findViewById(R.id.splash_layout)
+
+        setTheme()
         sharedPreferences = getPreferences(MODE_PRIVATE)
-        val view = findViewById<LinearLayout>(R.id.splash_layout)
-        view.alpha = 0f
-        view.animate().setDuration(Random.nextInt(1000, 2500).toLong()).alpha(1f).withEndAction{
+        viewLayout.alpha = 0f
+        viewLayout.animate().setDuration(Random.nextInt(1000, 2500).toLong()).alpha(1f).withEndAction{
             lifecycleScope.launch {
 
                 run tryToConnect@{
@@ -142,15 +121,73 @@ class SplashScreen : AppCompatActivity() {
                             }
                         } catch (e: Exception) {
                             startActivity(Intent(this@SplashScreen, AuthorizationActivity::class.java))
-                            finish()
+                            finishAffinity()
                         }
                     }
 
                     override fun onError(error: ANError) {
                         startActivity(Intent(this@SplashScreen, AuthorizationActivity::class.java))
-                        finish()
+                        finishAffinity()
                     }
                 })
+        }
+    }
+
+
+    private fun setTheme() {
+        when (Random.nextInt(1, 100)) {
+            in 1..25 -> { // 25%
+                mainLayout.setBackgroundColor(applicationContext.getColor(R.color.color_for_splash_screen1))
+                Glide.with(this)
+                    .load(R.drawable.splash_screen1)
+                    .format(DecodeFormat.PREFER_RGB_565)
+                    .into(logoImageView)
+                window.navigationBarColor = applicationContext.getColor(R.color.blue_700)
+                window.statusBarColor = applicationContext.getColor(R.color.blue_700)
+            }
+            in 26..50 -> { // 25%
+                mainLayout.setBackgroundColor(applicationContext.getColor(R.color.color_for_splash_screen2))
+                Glide.with(this)
+                    .load(R.drawable.splash_screen2)
+                    .format(DecodeFormat.PREFER_RGB_565)
+                    .into(logoImageView)
+                window.navigationBarColor = Color.parseColor("#000080")
+                window.statusBarColor = Color.parseColor("#000080")
+            }
+            in 51..75 -> { // 25%
+                mainLayout.setBackgroundColor(applicationContext.getColor(R.color.color_for_splash_screen3))
+                Glide.with(this)
+                    .load(R.drawable.splash_screen3)
+                    .format(DecodeFormat.PREFER_RGB_565)
+                    .into(logoImageView)
+                window.navigationBarColor = applicationContext.getColor(R.color.orange_700)
+                window.statusBarColor = applicationContext.getColor(R.color.orange_700)
+            }
+            in 76..85 -> { // 10%
+                mainLayout.setBackgroundColor(applicationContext.getColor(R.color.grey))
+                //viewLayout.removeView(logoImageView)
+                viewLayout.removeAllViews()
+                val textView = TextView(this)
+                textView.text = "Сам знаешь кто здесь должен быть"
+                textView.textSize = 24F
+                textView.gravity = Gravity.CENTER
+                textView.layoutParams = viewLayout.layoutParams
+                textView.setTextColor(ContextCompat.getColor(baseContext, R.color.dark_red))
+                viewLayout.addView(textView)
+                Snackbar.make(textView, "Сам знаешь кто здесь должен быть!!!", Snackbar.LENGTH_SHORT)
+                    .setAction("И кто же?") { startActivity(Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://vk.com/378508088"))) } //t.md63
+                    .show()
+                window.navigationBarColor = applicationContext.getColor(R.color.black)
+                window.statusBarColor = applicationContext.getColor(R.color.black)
+            }
+            in 86..100 -> { // 15%
+                mainLayout.setBackgroundColor(applicationContext.getColor(R.color.grey))
+                //viewLayout.removeView(logoImageView)
+                viewLayout.removeAllViews()
+                window.navigationBarColor = applicationContext.getColor(R.color.black)
+                window.statusBarColor = applicationContext.getColor(R.color.black)
+                ShowToast.show(baseContext, "Ничего пока не придумал")
+            }
         }
     }
 }
